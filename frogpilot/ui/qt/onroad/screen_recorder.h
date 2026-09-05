1ae44fd0a7b5dc70fc616979062b6fb9d0711b96
James <91348155+FrogAi@users.noreply.github.com>
2026-09-05 12:00:00 -0700

    Compile FrogPilot

--- frogpilot/ui/qt/onroad/screen_recorder.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class ScreenRecorder {
-public:
-  static void attach();  // call after the EGL display exists and before the first frame is swapped, when the swapchain is created
-  static void start();
-  static void stop();
-  static bool active();
-};
